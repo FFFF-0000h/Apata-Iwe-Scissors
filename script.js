a8a9fa98eb605ec65a2d15c63fd8215fd8807071
FFFF-0000h <adebiyiitunuayo@gmail.com>
2024-07-21 01:24:44 +0100

    Refactor code: ES6

--- script.js
+++ script.js
@@ -1,72 +1,69 @@
 function game() {
     const maxGames = 5; // max number of games to play
-    let computerstartPt = 0; // start point for the matrix
-    let playerstartPt = 0; // start point for player
-
-for (let i = 0; i < maxGames; i++) {
-let playerSelection = prompt("Choose your hand: Apata, Iwe, or Scissors").toLowerCase();
-function computerPlayer() {
-    let ais = ["Apata", "Iwe", "Scissors"];
-    let myAis = ais[Math.floor(Math.random() * ais.length)];
-    return myAis;
-    // Function to randomly generate a hand for the matrix
-}
-let computerSelection = computerPlayer();
-let overallRound = playRound(playerSelection, computerSelection);
-if (overallRound === "You Win! Iwe beats Apata") {
-    playerstartPt++;
-    console.log("You win");
-} else if (overallRound === "You Lose! Apata beats Scissors") {
-    computerstartPt++;
-    console.log("You lose");
-} else if (overallRound ===  "You Lose! Scissors beats Iwe") {
-    computerstartPt++;
-    console.log("You lose");
-} else if (overallRound === "You Lose! Iwe beats Apata") {
-    computerstartPt++;
-    console.log("You Lose");
-} else if (overallRound === "Game is Tied") {
-    console.log("Tied");
-} else if (overallRound === "You Win! Apata beats Scissors") {
-    playerstartPt++;
-    console.log("You win");
-} else if (overallRound === "Game is Tied") {
-    console.log("Tied");
-} else if (overallRound === "You Win! Scissors beats Iwe") {
-    playerstartPt++;
-    console.log("You win");
-} else {
-    console.log("Tied");
-} 
+    let computerStartPt = 0; // start point for the matrix
+    let playerStartPt = 0; // start point for player
+  
+    const choices = ["apata", "iwe", "scissors"];
+    
+    for (let i = 0; i < maxGames; i++) {
+      const playerSelection = prompt("Choose your hand: Apata, Iwe, or Scissors").toLowerCase();
+  
+      const computerPlayer = () => {
+        const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+        return randomChoice;
+      };
+  
+      const computerSelection = computerPlayer();
+      const overallRound = playRound(playerSelection, computerSelection);
+  
+      switch (overallRound) {
+        case "You Win! Iwe beats Apata":
+        case "You Win! Apata beats Scissors":
+        case "You Win! Scissors beats Iwe":
+          playerStartPt++;
+          console.log("You win");
+          break;
+        case "You Lose! Apata beats Scissors":
+        case "You Lose! Scissors beats Iwe":
+        case "You Lose! Iwe beats Apata":
+          computerStartPt++;
+          console.log("You lose");
+          break;
+        case "Game is Tied":
+        default:
+          console.log("Tied");
+          break;
+      }
     }
-     
-if (playerstartPt > computerstartPt) {
-    console.log("...");
-    console.log("....");
-    console.log("Congratulations! You beat the Matrix " + playerstartPt + " to " + computerstartPt);
-} else {
-    console.log("Try again. The Matrix beat you " + computerstartPt + " to " + playerstartPt);
-} 
-    }
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "apata" && computerSelection == "Iwe") {
-        return "You Lose! Iwe beats Apata";
-    } else if (playerSelection == "scissors" && computerSelection == "Apata") {
-        return "You Lose! Apata beats Scissors";
-    } else if (playerSelection == "iwe" && computerSelection == "Scissors") {
-        return "You Lose! Scissors beats Iwe";
-    } else if (playerSelection == "iwe" && computerSelection == "Apata") {
-        return "You Win! Iwe beats Apata";
-    } else if (playerSelection == "iwe" && computerSelection == "Iwe") {
-        return "Game is Tied";
-    } else if (playerSelection == "apata" && computerSelection == "Scissors") {
-        return "You Win! Apata beats Scissors";
-    } else if (playerSelection == "apata" && computerSelection == "Apata") {
-        return "Game is Tied";
-    } else if (playerSelection == "scissors" && computerSelection == "Iwe") {
-        return "You Win! Scissors beats Iwe";
+  
+    if (playerStartPt > computerStartPt) {
+      console.log("...");
+      console.log("....");
+      console.log(`Congratulations! You beat the Matrix ${playerStartPt} to ${computerStartPt}`);
     } else {
-        return "Game is Tied";
+      console.log(`Try again. The Matrix beat you ${computerStartPt} to ${playerStartPt}`);
     }
- }
+  }
+  
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "apata" && computerSelection === "iwe") {
+      return "You Lose! Iwe beats Apata";
+    } else if (playerSelection === "scissors" && computerSelection === "apata") {
+      return "You Lose! Apata beats Scissors";
+    } else if (playerSelection === "iwe" && computerSelection === "scissors") {
+      return "You Lose! Scissors beats Iwe";
+    } else if (playerSelection === "iwe" && computerSelection === "apata") {
+      return "You Win! Iwe beats Apata";
+    } else if (playerSelection === "iwe" && computerSelection === "iwe") {
+      return "Game is Tied";
+    } else if (playerSelection === "apata" && computerSelection === "scissors") {
+      return "You Win! Apata beats Scissors";
+    } else if (playerSelection === "apata" && computerSelection === "apata") {
+      return "Game is Tied";
+    } else if (playerSelection === "scissors" && computerSelection === "iwe") {
+      return "You Win! Scissors beats Iwe";
+    } else {
+      return "Game is Tied";
+    }
+  }
+  
